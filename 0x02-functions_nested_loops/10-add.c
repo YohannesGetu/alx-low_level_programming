ecdad54c4314ab72efd07a2962597a1e098032b6
Yohannes Getu <nede.evoli@gmail.com>
2021-06-22 04:36:35 -0700

    added a C program that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - addes to integer
+ * @num1: first integer argument
+ * @num2: second integer argument
+ * Return: the sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
