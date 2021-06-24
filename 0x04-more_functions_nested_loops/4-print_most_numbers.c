d9b4b69da7a120c6a8aba858d77af3e756734784
Yohannes Getu <nede.evoli@gmail.com>
2021-06-24 04:46:31 -0700

    added a C program that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		if (num == 4 || num == 2)
+		{
+			num++;
+			continue;
+		}
+		_putchar(num + '0');
+		num++;
+	}
+	_putchar('\n');
+}
