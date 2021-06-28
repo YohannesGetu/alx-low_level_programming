bdb96a0b47f2eeb2d6a6bc8f6f4234b900a64881
Yohannes Getu <nede.evoli@gmail.com>
2021-06-28 03:51:49 -0700

    added a C program that convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("          ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", nb);
+	return (0);
+}
