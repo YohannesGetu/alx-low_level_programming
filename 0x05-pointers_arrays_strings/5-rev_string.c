7874f3ccfbee1815b86a0cc203a7112218d55285
Yohannes Getu <nede.evoli@gmail.com>
2021-06-28 01:50:00 -0700

    added a C program that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer of char
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	i = 0;
+
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c~
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string,in reverse , followed by a new line
+ * @s: pointer of char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
