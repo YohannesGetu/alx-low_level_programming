beb8e18de2586b6f1950024d5f4ab9ec3d64bfa1
Yohannes Getu <nede.evoli@gmail.com>
2021-06-30 01:47:40 -0700

    added a C program  that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * *rot13 - function that encodes a string using rot13.
+ * @s: pointer pointed to the variable s , of type character
+ * Return: Pointer P , pointed to the variable s of type char.
+ */
+char *rot13(char *s)
+{
+	int i;
+	char *p = s;
+
+	char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (i < 53)
+		{
+			if (*s == original[i])
+			{
+				*s = code[i];
+				break;
+			}
+			i++;
+		}
+		s++;
+	}
+	return (p);
+}
