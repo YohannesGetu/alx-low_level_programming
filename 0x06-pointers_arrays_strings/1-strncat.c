7b6c7ed934906bb366d9074ef6c9cd0e989bc8e9
Yohannes Getu <nede.evoli@gmail.com>
2021-06-29 23:32:38 -0700

    added a C program that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strncat - function
+ *@dest: char
+ *@src: char
+ *@n: int
+ *Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
