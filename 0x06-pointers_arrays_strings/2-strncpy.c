d898cf5e9d9aeb912de1fd4a2a8d23dc948f576d
Yohannes Getu <nede.evoli@gmail.com>
2021-06-29 23:40:35 -0700

    added a C program that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include"holberton.h"
+/**
+ * _strncpy - copy string
+ * @src: pointer of char
+ * @dest: pointer of char
+ * @n: integer
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		for (; i < n; i++)
+
+			dest[i] = '\0';
+
+
+	}
+	return (dest);
+}
