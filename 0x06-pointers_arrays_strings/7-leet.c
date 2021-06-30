b08122a526e126d81dc7b5456b247104ae0a276b
Yohannes Getu <nede.evoli@gmail.com>
2021-06-30 01:17:55 -0700

    added a C program that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *leet - a function that encodes a string into 1337.
+ *@s: string to be crypted .
+ *Return: pointer to s .
+ *Update V 1.0 : In this new code , i changed 2 arrays with 2 pointers, that
+ *are pointed to the BASS adresses of the 2 arrays.
+ *in addition to that , i assigned a pointer p to s.
+ */
+
+char *leet(char *s)
+{
+	char *m = "aeotl";
+	char *M = "AEOTL";
+	int x[] = {'4', '3', '0', '7', '1'};
+	int i;
+	char *p = s;
+
+	while (*s != '\0')
+	{
+
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == *(m + i) || *s == *(M + i))
+
+				*s = x[i];
+		}
+		s++;
+
+	}
+	return (p);
+}
