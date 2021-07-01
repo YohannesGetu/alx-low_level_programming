9eb5111683d7913aee48942534a46baab6faa641
Yohannes Getu <61448882+YohannesGetu@users.noreply.github.com>
2021-07-01 09:01:27 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,102 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an integer.
- * @n: integer
- * Return: void
+ *powB - raises the number base to power power
+ *@base : the base
+ *@power : the power
+ *Return: return the answer
  */
-
-void print_number(int n)
+int powB(unsigned int base, int power)
 {
-	int divisor = 1, i, resp;
+	int i, prod = 1;
+
+	for (i = 0; i < power; i++)
+	{
+		prod = prod * base;
+	}
+	return (prod);
+}
+
+/**
+ *numLength - returns the lenth of string
+ *@num : operand number
+ *Return: number of digits
+ */
+int numLength(unsigned int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+
+/**
+ *printnchar - Print a number of any digit
+ *@n : takes an input number
+ *
+ *
+ */
+void printnchar(int n)
+{
+	int length, j, k, digit1, tmp2;
+	unsigned int num;
 
 	if (n < 0)
 	{
-		_putchar('-');
 		n *= -1;
+		_putchar('-');
 	}
-
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	num = n;
+	length = numLength(num), j = length - 1, k, digit1, tmp2;
+	if (num == 0)
+		_putchar(48);
+	else
 	{
-		resp = n / divisor;
-		_putchar('0' + resp);
+		while (j >= 0)
+		{
+			if (num % powB(10, j) == 0 && j != 0)
+			{
+				_putchar(48 + num / powB(10, j));
+				for (k = j; k > 0; k--)
+					_putchar(48);
+				j = -1;
+			}
+			else
+			{
+				digit1 = num / powB(10, j);
+				_putchar(digit1 + 48);
 
+				tmp2 = num;
+				num -= powB(10, j) * digit1;
+				if (numLength(tmp2) - numLength(num) == 2)
+				{
+					_putchar(48);
+					j--;
+				}
+				j--;
+			}
+		}
 	}
+}
 
+
+
+
+/**
+ * print_number - prints out any integer with only _putchar
+ * @n: integer to be printed
+ * Return: 0
+ */
+void print_number(int n)
+{
+
+	printnchar(n);
 }
