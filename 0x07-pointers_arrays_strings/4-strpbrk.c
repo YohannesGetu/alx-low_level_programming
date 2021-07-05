4296c35f2ca714b3fa8b5be85f0190f4c498787f
Yohannes Getu <nede.evoli@gmail.com>
2021-07-04 23:40:21 -0700

    added a C program that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: char to check
+ * @accept: char to look for
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+			if (s[i] == accept[k])
+				return (s + i);
+		}
+	}
+	return (0);
+}
