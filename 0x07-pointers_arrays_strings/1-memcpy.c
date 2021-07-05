33f322bc98ec8e4458a4785f5fdf1e43ed9eeb1f
Yohannes Getu <nede.evoli@gmail.com>
2021-07-04 23:26:14 -0700

    added a C program that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - fnct to copy memory content source to a char destination .
+ * @dest: memory allocation of the destination
+ * @src: memory content to copy from the src
+ * @n: content in byte to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,5 +5,6 @@
 
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* HOLBERTON_H */
