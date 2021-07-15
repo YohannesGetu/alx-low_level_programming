205bb01107f6248fda6f28974566c462c477dcc7
Yohannes Getu <nede.evoli@gmail.com>
2021-07-15 13:15:07 -0700

    added a C program that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -8,5 +8,6 @@ int _putchar(char c);
 int _strlen(char *);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* HOLBERTON_H */
