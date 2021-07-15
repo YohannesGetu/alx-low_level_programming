5af089c0b93f3e7dbef04d698652060893d40512
Yohannes Getu <61448882+YohannesGetu@users.noreply.github.com>
2021-07-15 23:48:31 +0300

    Update holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,17 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef HOLBERTON
+#define HOLBERTON
 
 int _putchar(char c);
-int _strlen(char *);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* HOLBERTON_H */
+#endif
