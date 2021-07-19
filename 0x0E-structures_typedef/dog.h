379b612d81f86919e24ec2d86a449876bc0fa31e
Yohannes Getu <nede.evoli@gmail.com>
2021-07-19 01:26:32 -0700

    added a C program that Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Create a struct of a dog
+ * @name: Dog's name
+ * @age: Age of the dog
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
