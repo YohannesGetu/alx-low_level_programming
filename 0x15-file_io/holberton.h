d930e1f289099a266ad23c94c623af14ff201103
Yohannes Getu <nede.evoli@gmail.com>
2021-08-08 22:00:44 -0700

    added a C program that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _HOLBERTON_H_ */
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _MAIN_H_ */
